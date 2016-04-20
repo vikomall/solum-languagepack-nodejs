badb8ec9eb83f4d05665c0cd0d958d079ecba85e
Vijendar Komalla <vikomall@users.noreply.github.com>
2016-04-20 14:18:43 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ cd /app
 su nodeuser -c -l "npm install grunt-cli --save -g"
 su nodeuser -c -l "npm install bower --save -g"
 su nodeuser -c -l "npm install"
-#su nodeuser -c -l "bower install --allow-root"
+su nodeuser -c -l "bower install --allow-root"
 #su nodeuser -c -l "grunt build"
